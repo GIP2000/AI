0e23aecc47c364186bdaaeafdb1119c6ca18cf3a
Gregory Presser <gpress2222@gmail.com>
2022-10-12 16:29:24 -0400

    Moved max/min to different functions for clarity

--- checkers/src/ai/mod.rs
+++ checkers/src/ai/mod.rs
@@ -3,6 +3,25 @@ use std::i32::MAX;
 use std::time::SystemTime;
 const MIN:i32 = -MAX;
 
+#[derive(Clone,Copy)]
+enum ABResultType{
+    TimeLimitExpired,
+    DepthReached,
+    Finished,
+    Inital,
+}
+
+enum EndEarly {
+    Yes((i32, ABResult)),
+    No
+}
+
+struct ABResult {
+    r#type: ABResultType,
+    value: Option<usize>
+}
+
+
 pub fn predict_move(b: Board, time_limit: u32) -> usize {
     let player_info_rc = b.get_player_info();
     let player_info = player_info_rc.borrow();
@@ -17,20 +36,137 @@ pub fn predict_move(b: Board, time_limit: u32) -> usize {
     println!("Starting AB/P");
     let now = SystemTime::now();
     loop {
-        println!("Starting depth {:}, time is {:}ms", d, now.elapsed().expect("Err: Invalid Sys time").as_millis());
-        match ab_prune_d(b.clone(), d, MIN, MAX,true, ((time_limit as u128) * 1000) - 100, &now).1 {
-            Some(v) => {
-                mv = v;
+        // println!("Starting depth {:}, time is {:}ms", d, now.elapsed().expect("Err: Invalid Sys time").as_millis());
+        let (_, v) = max_value(b.clone(), d, MIN, MAX, ((time_limit as u128) * 1000) - 100, &now);
+        match v.r#type {
+            ABResultType::Finished => {
+                println!("Found Bottom Depth: {:?}", d);
+                return v.value.expect("Err: Finished without value");
             },
-            None => {
-                println!("Done Pruning it took {:}ms",now.elapsed().expect("Err: Invalid Sys time").as_millis());
+            ABResultType::TimeLimitExpired => {
+                println!("Time limit expired in depth {:?}, curreint time is {:?}", d, now.elapsed().expect("Err: Invalid Sys time").as_millis());
                 return mv;
+            },
+            ABResultType::DepthReached => {
+                println!("Finishe depth {:?}, curreint time is {:?}", d, now.elapsed().expect("Err: Invalid Sys time").as_millis());
+                mv = v.value.expect("Err: No DepthReached without value");
+            },
+            ABResultType::Inital => {
+                panic!("Unreachable Inital value");
             }
-        }
+        };
         d+=1;
     }
+}
 
 
+fn is_terminal(state: &Board, depth: u32, time_limit: u128, now: &SystemTime, is_max: bool) -> EndEarly {
+    if now.elapsed().expect("Err: Invalid Sys time").as_millis() >= time_limit {
+        return EndEarly::Yes((0, ABResult{
+            r#type: ABResultType::TimeLimitExpired,
+            value: None
+        }));
+    }
+    let (is_game_over, is_tie_op, winner) = state.is_game_over();
+    if is_game_over {
+        let (is_tie, winner) = (is_tie_op.unwrap(), winner.unwrap());
+        if is_tie {
+            return EndEarly::Yes((0, ABResult{
+                r#type: ABResultType::Finished,
+                value: None
+            }));
+        }
+        if (winner == state.get_current_player() && is_max) || (winner != state.get_current_player() && !is_max) {
+            return EndEarly::Yes((MAX, ABResult{
+                r#type: ABResultType::Finished,
+                value: None
+            }));
+        }
+        return EndEarly::Yes((MIN, ABResult{
+            r#type: ABResultType::Finished,
+            value: None
+        }));
+    }
+    if depth == 0 {
+        return EndEarly::Yes((h(&state, is_max), ABResult {
+            r#type: ABResultType::DepthReached,
+            value: None
+        }));
+    }
+    EndEarly::No
+
+}
+
+fn max_value(state: Board, depth: u32, mut alpha: i32, beta: i32, time_limit: u128, now: &SystemTime) -> (i32, ABResult){
+    match is_terminal(&state, depth, time_limit, now, true) {
+        EndEarly::Yes(r) => return r,
+        EndEarly::No => {}
+    };
+
+    let mut v = MIN;
+    let mut mv = ABResult {
+        r#type: ABResultType::Inital,
+        value: Some(0)
+    };
+    for p_mv in 0..state.get_player_info().borrow().get_moves().len() {
+        let mut new_state = state.clone();
+        new_state.do_move(p_mv);
+        let (v2, t_move) = min_value(new_state,depth-1,alpha,beta,time_limit,now);
+        // should I update stuff
+        if v2 > v {
+            v = v2;
+            mv.r#type = t_move.r#type;
+            mv.value = Some(p_mv);
+            if v > alpha {
+                alpha = v;
+            }
+        }
+        // time limit expired get out
+        if let ABResultType::TimeLimitExpired = t_move.r#type {
+            return (v, t_move);
+        }
+        // should I prune
+        if v >= beta {
+            return (v,mv);
+        }
+    }
+    return (v,mv);
+
+}
+fn min_value(state: Board, depth: u32, alpha: i32, mut beta: i32, time_limit: u128, now: &SystemTime) -> (i32, ABResult){
+    match is_terminal(&state, depth, time_limit, now, false) {
+        EndEarly::Yes(r) => return r,
+        EndEarly::No => {}
+    };
+
+    let mut v = MAX;
+    let mut mv = ABResult {
+        r#type: ABResultType::Inital,
+        value: Some(0)
+    };
+    for p_mv in 0..state.get_player_info().borrow().get_moves().len() {
+        let mut new_state = state.clone();
+        new_state.do_move(p_mv);
+        let (v2,t_move) = max_value(new_state,depth-1, alpha,beta,time_limit, now);
+        if v2 < v {
+            v = v2;
+            mv.r#type = t_move.r#type;
+            mv.value = Some(p_mv);
+            if v < beta {
+                beta = v
+            }
+        }
+
+        // time limit expired get out
+        if let ABResultType::TimeLimitExpired = t_move.r#type {
+            return (v, t_move);
+        }
+
+        if v <= alpha {
+            return (v,mv);
+        }
+    }
+    return (v,mv);
 }
 
 fn h(state:&Board, is_max: bool) -> i32 {
@@ -58,61 +194,4 @@ fn h(state:&Board, is_max: bool) -> i32 {
     } else {
         -score
     }
-}
-
-fn ab_prune_d(state: Board, depth: u32, mut alpha: i32, mut beta: i32, is_max: bool, time_limit: u128, now: &SystemTime) -> (i32,Option<usize>) {
-    if now.elapsed().expect("Err: Invalid Sys time").as_millis() >= time_limit {
-        return (0, None);
-    }
-
-    let (is_game_over, is_tie_op, winner) = state.is_game_over();
-    if is_game_over {
-        let (is_tie, winner) = (is_tie_op.unwrap(), winner.unwrap());
-        if is_tie {
-            return (0, None);
-        }
-        if (winner == state.get_current_player() && is_max) || winner != state.get_current_player() && !is_max{
-            return (MAX, None);
-        }
-        return (MIN, None);
-    }
-    if depth == 0 {
-        return (h(&state, is_max), None);
-    }
-
-    let mut v = MIN;
-    let mut mv = 0;
-    for p_mv in 0..state.get_player_info().borrow().get_moves().len() {
-        let mut new_state = state.clone();
-        new_state.do_move(p_mv);
-        let v2 = ab_prune_d(new_state,depth-1, alpha, beta, !is_max, time_limit, now).0;
-        if (is_max && v2 > v) || (!is_max && v2 < v) {
-            (v, mv) = (v2, p_mv);
-            match is_max {
-                true => {
-                    if v > alpha {
-                        alpha = v;
-                    }
-                },
-                false => {
-                    if v < beta {
-                        beta = v;
-                    }
-                }
-            };
-        }
-        match is_max {
-            true=> {
-                if v >= beta {
-                    return (v,Some(mv));
-                }
-            },
-            false => {
-                if v <= alpha {
-                    return (v,Some(mv));
-                }
-            }
-        }
-    }
-    (v,Some(mv))
 }
